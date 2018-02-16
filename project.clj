47582a58bc7814112c781cf2794b9dc9e1438c5d
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2018-02-16 10:52:11 +0100

    added reagent

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
 
   :dependencies [[org.clojure/clojure "1.9.0-beta4"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async  "0.3.443"]]
+                 [org.clojure/core.async  "0.3.443"]
+                 [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]]
 
   :plugins [[lein-figwheel "0.5.14"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
