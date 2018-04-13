cb81e58b95fd99185cdf5ef4014f8ff845d01c04
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2018-04-13 11:20:59 +0200

    text styles docs

--- src/rn_styles/core.cljs
+++ src/rn_styles/core.cljs
@@ -4,28 +4,28 @@
 
 (def react (js/require "react-native"))
 
-(defn create-styles [s]
+(defn- create-styles [s]
   (let [s1 (reduce #(assoc %1 (%2 0) (ru/camelify-map-keys (%2 1))) {} s)]
     (.create (.-StyleSheet react) (clj->js s1))))
 
-(def styles (atom {}))
-(def original-styles (atom {}))
+(def ^:private styles (atom {}))
+(def ^:private original-styles (atom {}))
 
-(defn get-stylesheet [domain]
+(defn- get-stylesheet [domain]
   (domain @styles))
 
-(defn add-stylesheet [domain new-styles]
+(defn- add-stylesheet [domain new-styles]
   (if-let [exists (get-stylesheet domain)]
     exists
     (do
       (reset! styles (assoc @styles domain (create-styles new-styles)))
       (get-stylesheet domain))))
 
-(defn get-style [style-name]
+(defn- get-style [style-name]
   (when-let [sheet (get-stylesheet style-name)]
     (aget sheet (name style-name))))
 
-(defn new-style [style-name style]
+(defn- new-style [style-name style]
   (if-let [exists (get-style style-name)]
     exists
     (do
@@ -34,10 +34,12 @@
         (reset! original-styles (assoc @original-styles style-id style))
         style-id))))
 
-(defn original [style-id]
+(defn- original [style-id]
   (get @original-styles style-id))
 
-(defn print-style [& style]
+(defn print-style
+  "Утилитная функция - выводит на консоль стиль или список стилей в формате map"
+  [& style]
   (println)
   (println)
   (println)
@@ -57,47 +59,79 @@
 
 ; layout
 
-(defn display [v]
+(defn display
+  ""
+  [v]
   [:flex :none]
   (let [v (name v)]
     (new-style (keyword (str "display-" v)) {:display v})))
 
 (defn flex
+  ""
   ([] (flex 1))
   ([v]
    (new-style (keyword (str "flex-" v)) {:flex v})))
 
-(defn align-self [v]
+(defn align-self
+  ""
+  [v]
   (new-style (keyword (str "align-self-" v)) {:align-self v}))
 
-(defn align-items [v]
+(defn align-items
+  ""
+  [v]
   (new-style (keyword (str "align-items-" v)) {:align-items v}))
 
-(defn flex-align [v]
+(defn flex-align
+  ""
+  [v]
   (new-style (keyword (str "flex-align-" v)) {:align-items     v
                                               :justify-content v}))
 
-(defn justify-content [v]
+(defn justify-content
+  ""
+  [v]
   (new-style (keyword (str "justify-content-" v)) {:justify-content v}))
 
-(defn flex-wrap [v]
+(defn flex-wrap
+  ""
+  [v]
   (new-style (keyword (str "flex-wrap-" v)) {:flex-wrap v}))
 
-(def row (new-style :row {:flex-direction "row"}))
-(def column (new-style :column {:flex-direction "column"}))
-(def align-center (align-items "center"))
-(def align-right (align-items "flex-end"))
-(def stretch (align-items "stretch"))
-(def justify-center (justify-content "center"))
-(def wrap (new-style :flex-wrap {:flex-wrap "wrap"}))
-(def no-wrap (new-style :flex-wrap {:flex-wrap "nowrap"}))
+(def row
+  ""
+  (new-style :row {:flex-direction "row"}))
+(def column
+  ""
+  (new-style :column {:flex-direction "column"}))
+(def align-center
+  ""
+  (align-items "center"))
+(def align-right
+  ""
+  (align-items "flex-end"))
+(def stretch
+  ""
+  (align-items "stretch"))
+(def justify-center
+  ""
+  (justify-content "center"))
+(def wrap
+  ""
+  (new-style :flex-wrap {:flex-wrap "wrap"}))
+(def no-wrap
+  ""
+  (new-style :flex-wrap {:flex-wrap "nowrap"}))
 
 ; background color
 
-(defn gray-cl [opacity]
+(defn gray-cl
+  ""
+  [opacity]
   (str "rgba(0,0,0,0." opacity ")"))
 
 (defn gray
+  ""
   ([] (gray 2))
   ([opacity]
    (new-style
@@ -108,72 +142,110 @@
   (str "rgba(255,255,255,0." opacity ")"))
 
 (defn white
+  ""
   ([]  (white 999))
   ([opacity]
    (new-style
     (keyword  (str "white-" opacity))
     {:background-color (white-cl opacity)})))
 
-(defn background [v]
+(defn background
+  ""
+  [v]
   (new-style (keyword (str "background-color-" v)) {:background-color v}))
-
-(def nil-color "rgba(0,0,0,0)")
 
 ;; text styles
 
-(defn color [v]
+(defn color
+  ""
+  [v]
   (new-style (keyword (str "color-" v)) {:color v}))
 
-(defn font-style [v]
+(defn font-style
+  "enum('normal', 'italic'"
+  [v]
   (new-style (keyword (str "font-style" v)) {:font-style v}))
 
-(defn font-family [v]
+(defn font-family
+  ""
+  [v]
   (new-style (keyword (str "font-family" v)) {:font-family v}))
 
-(defn font-weight [v]
+(defn font-weight
+  "Specifies font weight. The values 'normal' and 'bold' are supported for most fonts. Not all fonts have a variant for each of the numeric values, in that case the closest one is chosen."
+  [v]
   (new-style (keyword (str "font-weight" v)) {:font-weight v}))
 
-(defn line-height [v]
+(defn line-height
+  ""
+  [v]
   (new-style (keyword (str "line-height" v)) {:line-height v}))
 
-(defn text-align-vertical [v] ; android
+(defn text-align-vertical
+  "(Android) enum('auto', 'top', 'bottom', 'center')"
+  [v]
   (new-style (keyword (str "text-align-vertical" v)) {:text-align-vertical v}))
 
-(defn leter-spacing [v] ; ios
+(defn font-variant
+  "(IOS) array of enum('small-caps', 'oldstyle-nums', 'lining-nums', 'tabular-nums', 'proportional-nums')"
+  [v]
+  (new-style (keyword (str "font-variant-" v)) {:font-variant v}))
+
+(defn leter-spacing
+  "(IOS)"
+  [v]
   (new-style (keyword (str "leter-spacing" v)) {:leter-spacing v}))
 
-(defn text-decoration-color [v] ; ios
+(defn text-decoration-color
+  "(IOS)"
+  [v]
   (new-style (keyword (str "text-decoration-color" v)) {:text-decoration-color v}))
 
 (defn text-decoration-line
-  "'none', 'underline', 'line-through'"
+  ""
   [v]
   (new-style (keyword (str "text-decoration-line" v)) {:text-decoration-line v}))
 
-(def underline (text-decoration-line "underline"))
-(def line-through (text-decoration-line "line-through"))
+(def underline
+  "text-decoration undeline"
+  (text-decoration-line "underline"))
+
+(def line-through
+  "enum('none', 'underline', 'line-through', 'underline line-through')"
+  (text-decoration-line "line-through"))
 
 (defn text-decoration-style
-  "'solid', 'double', 'dotted', 'dashed'"
+  "(IOS) enum('solid', 'double', 'dotted', 'dashed')"
   [v]
   (new-style (keyword (str "text-decoration-style" v)) {:text-decoration-style v}))
 
 (defn writing-direction
-  "'auto', 'ltr', 'rtl'"
+  "(IOS) enum('auto', 'ltr', 'rtl')"
   [v]
   (new-style (keyword (str "writing-direction" v)) {:writing-direction v}))
 
-(def bold (font-weight "bold"))
+(def bold
+  "font-weight 'bold'"
+  (font-weight "bold"))
 
-(def italic (font-style "italic"))
+(def italic
+  "font-weight 'italic'"
+  (font-style "italic"))
 
-(defn font-size [v]
+(defn font-size
+  ""
+  [v]
   (new-style (keyword (str "font-size-" v)) {:font-size v}))
 
-(defn text-align [v]
+(defn text-align
+  "Specifies text alignment. The value 'justify' is only supported on iOS and fallbacks to left on Android.
+
+  enum('auto', 'left', 'right', 'center', 'justify')"
+  [v]
   (new-style (keyword (str "text-align-" v)) {:text-align v}))
 
 (defn text-shadow
+  ""
   ([] (text-shadow 0 2 5 2))
   ([w h r a]
    (new-style (keyword (str "text-shadow-" a "-" w "-" h "-" r))
@@ -181,24 +253,38 @@
                :text-shadow-radius r
                :text-shadow-color  (str "rgba(0,0,0,0." a ")")})))
 
+(defn include-font-padding
+  "(Android) Set to false to remove extra font padding intended to make space for certain ascenders / descenders. With some fonts, this padding can make text look slightly misaligned when centered vertically. For best results also set textAlignVertical to center. Default is true."
+  [v]
+  (new-style (keyword (str "include-font-padding-" v)) {:include-font-padding v}))
+
 ;; dimentions
 
-(defn height [v]
+(defn height
+  ""
+  [v]
   (new-style (keyword (str "height-" v)) {:height v}))
 
-(defn width [v]
+(defn width
+  ""
+  [v]
   (new-style (keyword (str "width-" v)) {:width v}))
 
-(defn size [w h]
+(defn size
+  ""
+  [w h]
   (new-style (keyword (str "width-" w "-height-" h)) {:width w :height h}))
 
-(defn layout->size [layout]
+(defn layout->size
+  ""
+  [layout]
   (new-style (keyword (str "width-" (:widht layout) "-height-" (:height layout)))
              (select-keys layout [:width :height])))
 
 ;; borders
 
 (defn rounded
+  ""
   ([] (rounded 8))
   ([v]
    (new-style (keyword (str "rounded-" v))
@@ -212,6 +298,7 @@
                :border-top-left-radius     tl})))
 
 (defn border
+  ""
   ([] (border 1))
   ([w] (border w "rgba(0,0,0,0.4)"))
   ([w c] (border w c "solid"))
@@ -222,6 +309,7 @@
                :border-color c})))
 
 (defn border-bottom
+  ""
   ([] (border-bottom 1))
   ([w] (border-bottom w "rgba(0,0,0,0.4)"))
   ([w c]
@@ -230,6 +318,7 @@
                :border-bottom-color c})))
 
 (defn border-top
+  ""
   ([] (border-top 1))
   ([w] (border-top w "rgba(0,0,0,0.4)"))
   ([w c]
@@ -238,6 +327,7 @@
                :border-top-color c})))
 
 (defn border-left
+  ""
   ([] (border-left 1))
   ([w] (border-left w "rgba(0,0,0,0.4)"))
   ([w c]
@@ -246,6 +336,7 @@
                :border-left-color c})))
 
 (defn border-right
+  ""
   ([] (border-right 1))
   ([w] (border-right w "rgba(0,0,0,0.4)"))
   ([w c]
@@ -254,9 +345,11 @@
                :border-right-color c})))
 
 (defn elevation [v]
+  ""
   (new-style (keyword (str "elevation-" v)) {:elevation v}))
 
 (defn shadow
+  ""
   ([] (shadow 4))
   ([h] (shadow 0 h))
   ([w h] (shadow w h h))
@@ -273,6 +366,7 @@
 ;; box
 
 (defn padding
+  ""
   ([] (padding 8))
   ([v] (padding v v))
   ([t r] (padding t r t r))
@@ -283,19 +377,28 @@
                :padding-bottom b
                :padding-left   l})))
 
-(defn padding-horizontal [v]
+(defn padding-horizontal
+  ""
+  [v]
   (new-style (keyword (str "padding-horizontal-" v)) {:padding-horizontal v}))
 
-(defn padding-vertical [v]
+(defn padding-vertical
+  ""
+  [v]
   (new-style (keyword (str "padding-vertical-" v)) {:padding-vertical v}))
 
-(defn margin-horizontal [v]
+(defn margin-horizontal
+  ""
+  [v]
   (new-style (keyword (str "margin-horizontal-" v)) {:margin-horizontal v}))
 
-(defn margin-vertical [v]
+(defn margin-vertical
+  ""
+  [v]
   (new-style (keyword (str "margin-vertical-" v)) {:margin-vertical v}))
 
 (defn margin
+  ""
   ([] (margin 8))
   ([v] (margin v v))
   ([t r] (margin t r t r))
@@ -306,135 +409,217 @@
                :margin-bottom b
                :margin-left   l})))
 
-(defn margin-top [v]
+(defn margin-top
+  ""
+  [v]
   (new-style (keyword (str "margin-top-" v)) {:margin-top v}))
 
-(defn margin-left [v]
+(defn margin-left
+  ""
+  [v]
   (new-style (keyword (str "margin-left-" v)) {:margin-left v}))
 
-(defn margin-bottom [v]
+(defn margin-bottom
+  ""
+  [v]
   (new-style (keyword (str "margin-bottom-" v)) {:margin-bottom v}))
 
-(defn margin-right [v]
+(defn margin-right
+  ""
+  [v]
   (new-style (keyword (str "margin-right-" v)) {:margin-right v}))
 
-(defn padding-top [v]
+(defn padding-top
+  ""
+  [v]
   (new-style (keyword (str "padding-top-" v)) {:padding-top v}))
 
-(defn padding-left [v]
+(defn padding-left
+  ""
+  [v]
   (new-style (keyword (str "padding-left-" v)) {:padding-left v}))
 
-(defn padding-bottom [v]
+(defn padding-bottom
+  ""
+  [v]
   (new-style (keyword (str "padding-bottom-" v)) {:padding-bottom v}))
 
-(defn padding-right [v]
+(defn padding-right
+  ""
+  [v]
   (new-style (keyword (str "padding-right-" v)) {:padding-right v}))
 
 ;; position
 
-(defn position [v]
+(defn position
+  ""
+  [v]
   (new-style (keyword (str "position-" v)) {:position v}))
 
-(def position-absolute (position "absolute"))
+(def position-absolute
+  ""
+  (position "absolute"))
 
-(defn top [v]
+(defn top
+  ""
+  [v]
   (new-style (keyword (str "top-" v)) {:top v}))
 
-(defn left [v]
+(defn left
+  ""
+  [v]
   (new-style (keyword (str "left-" v)) {:left v}))
 
-(defn bottom [v]
+(defn bottom
+  ""
+  [v]
   (new-style (keyword (str "bottom-" v)) {:bottom v}))
 
-(defn right [v]
+(defn right
+  ""
+  [v]
   (new-style (keyword (str "right-" v)) {:right v}))
 
-(def box [(top 0) (left 0) (bottom 0) (right 0) position-absolute])
+(def box
+  "A very common pattern is to create overlays with position absolute and zero positioning."
+  [(top 0) (left 0) (bottom 0) (right 0) position-absolute])
 
-(defn z-index [v]
+(defn z-index
+  ""
+  [v]
   (new-style (keyword (str "z-index-" v)) {:z-index v}))
 
 ;; transform
 
-(defn transform [v]
+(defn transform
+  ""
+  [v]
   (new-style (keyword (str "transform-" v)) {:transform v}))
 
 ;; view
 
-(defn overflow [v]
+(defn overflow
+  ""
+  [v]
   (new-style (keyword (str "overflow-" v)) {:overflow v}))
 
-(defn opacity [v]
+(defn opacity
+  ""
+  [v]
   (new-style (keyword (str "opacity-" v)) {:opacity v}))
 
-(defn border-width [v]; enum('visible', 'hidden')
+(defn border-width
+  ""
+  [v] ; enum('visible', 'hidden')
   (new-style (keyword (str "border-width-" v)) {:border-width v}))
 
-(defn border-style [v]
+(defn border-style
+  ""
+  [v]
   (new-style (keyword (str "border-style-" v)) {:border-style v}))
 
-(defn backface-visibility [v] ;enum('visible', 'hidden')
+(defn backface-visibility
+  ""
+  [v] ;enum('visible', 'hidden')
   (new-style (keyword (str "backface-visibility-" v)) {:backface-visibility v}))
 
-(defn background-color [v]
+(defn background-color
+  ""
+  [v]
   (new-style (keyword (str "background-color-" v)) {:background-color v}))
 
-(defn border-top-color [v]
+(defn border-top-color
+  ""
+  [v]
   (new-style (keyword (str "border-top-color-" v)) {:border-top-color v}))
 
-(defn border-right-color [v]
+(defn border-right-color
+  ""
+  [v]
   (new-style (keyword (str "border-right-color-" v)) {:border-right-color v}))
 
-(defn border-bottom-color [v]
+(defn border-bottom-color
+  ""
+  [v]
   (new-style (keyword (str "border-bottom-color-" v)) {:border-bottom-color v}))
 
-(defn border-left-color [v]
+(defn border-left-color
+  ""
+  [v]
   (new-style (keyword (str "border-left-color-" v)) {:border-left-color v}))
 
-(defn border-top-width [v]
+(defn border-top-width
+  ""
+  [v]
   (new-style (keyword (str "border-top-width-" v)) {:border-top-width v}))
 
-(defn border-right-width [v]
+(defn border-right-width
+  ""
+  [v]
   (new-style (keyword (str "border-right-width-" v)) {:border-right-width v}))
 
-(defn border-bottom-width [v]
+(defn border-bottom-width
+  ""
+  [v]
   (new-style (keyword (str "border-bottom-width-" v)) {:border-bottom-width v}))
 
-(defn border-left-width [v]
+(defn border-left-width
+  ""
+  [v]
   (new-style (keyword (str "border-left-width-" v)) {:border-left-width v}))
 
-(defn border-bottom-left-radius [v]
+(defn border-bottom-left-radius
+  ""
+  [v]
   (new-style (keyword (str "border-bottom-left-radius-" v)) {:border-bottom-left-radius v}))
 
-(defn border-bottom-right-radius [v]
+(defn border-bottom-right-radius
+  ""
+  [v]
   (new-style (keyword (str "border-bottom-right-radius-" v)) {:border-bottom-right-radius v}))
 
-(defn border-top-left-radius [v]
+(defn border-top-left-radius
+  ""
+  [v]
   (new-style (keyword (str "border-top-left-radius-" v)) {:border-top-left-radius v}))
 
-(defn border-top-right-radius [v]
+(defn border-top-right-radius
+  ""
+  [v]
   (new-style (keyword (str "border-top-right-radius-" v)) {:border-top-right-radius v}))
 
 ;; image
 
-(defn border-color [v]
+(defn border-color
+  ""
+  [v]
   (new-style (keyword (str "border-color-" v)) {:border-color v}))
 
-(defn border-radius [v]
+(defn border-radius
+  ""
+  [v]
   (new-style (keyword (str "border-radius" v)) {:border-radius v}))
 
-(defn resize-mode [v]
+(defn resize-mode
+  ""
+  [v]
   (new-style (keyword (str "resize-mode" v)) {:resize-mode v}))
 
-(defn overlay-color [v]
+(defn overlay-color
+  ""
+  [v]
   (new-style (keyword (str "overlay-color" v)) {:overlay-color v}))
 
-(defn tint-color [v]
+(defn tint-color
+  ""
+  [v]
   (new-style (keyword (str "tint-color" v)) {:tint-color v}))
 
 ;; utils
 
-(defn cyrcle [{:keys [border border-color background radius]}]
+(defn circle
+  ""
+  [{:keys [border border-color background radius]}]
   (let [w (* 2 radius)]
     [align-center justify-center
      (overflow "hidden")
@@ -444,12 +629,20 @@
      (rounded (/ w 2))
      (when (or border-color (not background)) (border (or border 1) color))]))
 
-(def rotate-45 (transform [{:rotate :45deg}]))
-(def rotate-90 (transform [{:rotate :90deg}]))
-(def rotate-180 (transform [{:rotate :180deg}]))
+(def rotate-45
+  ""
+  (transform [{:rotate :45deg}]))
+(def rotate-90
+  ""
+  (transform [{:rotate :90deg}]))
+(def rotate-180
+  ""
+  (transform [{:rotate :180deg}]))
 (def rotate-270 (transform [{:rotate :270deg}]))
 
-(defn merge-styles [style-name & styles]
+(defn merge-styles
+  ""
+  [style-name & styles]
   (map-indexed (fn [index style]
                  (if (map? style)
                    (new-style (keyword (str style-name "-" index)) style)
